0e148553b7b9b2be197ab8228a9be982b898027b
hamza El idrissi <hamzaelidrissidafali@gmail.com>
2020-11-29 11:06:27 +0100

    first commit for integration test

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,25 +1,38 @@
 package guru.springframework.services;
 
 
+import guru.springframework.controllers.IndexController;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
  * Created by jt on 6/17/17.
  */
 public class RecipeServiceImplTest {
 
-    RecipeServiceImpl recipeService;
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
 
     @Mock
     RecipeRepository recipeRepository;
@@ -29,21 +42,36 @@ public class RecipeServiceImplTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        recipeService = new RecipeServiceImpl(recipeRepository);
+
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMvcMockito() throws Exception {
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isBadGateway())
+                .andExpect(view().name("index"));
     }
 
     @Test
     public void getRecipes() throws Exception {
 
         Recipe recipe = new Recipe();
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(1L);
+
         HashSet receipesData = new HashSet();
         receipesData.add(recipe);
+
+        receipesData.add(recipe2);
 
         when(recipeService.getRecipes()).thenReturn(receipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
-        assertEquals(recipes.size(), 1);
+        assertEquals(recipes.size(), 2);
         verify(recipeRepository, times(1)).findAll();
     }
 
